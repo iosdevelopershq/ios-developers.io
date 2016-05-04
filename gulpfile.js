259c9d9c866ee8e7eb45d018541a7e8d2d9c37f8
James Martinez <jamescmartinez@gmail.com>
2016-05-03 23:21:12 -0700

    Add Sass support

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 const gulp = require('gulp')
 const hb = require('gulp-hb')
 const layouts = require('handlebars-layouts')
+const sass = require('gulp-sass')
 
 gulp.task('default', ['build'], () => {
   gulp.watch('./src/**/*', ['build'])
 })
 
-gulp.task('build', ['handlebars'])
+gulp.task('build', ['handlebars', 'sass'])
 
 gulp.task('handlebars', () => {
   const hbStream = hb()
@@ -17,4 +18,11 @@ gulp.task('handlebars', () => {
   .src('./src/*.html')
   .pipe(hbStream)
   .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('sass', () => {
+  return gulp
+  .src('./src/sass/application.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./dist/css'))
 })
