abb3a81401795a1d7a69b84ba6d3dc9f408d289c
James Martinez <jamescmartinez@gmail.com>
2016-05-04 23:19:44 -0700

    Shuffle team member profile images on load

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,21 @@ $('.down-arrow').click(() => {
     scrollTop: $('#team').offset().top
   }, 500)
 })
+
+function shuffle(a) {
+  let j
+  let x
+  let i
+  for (i = a.length; i; i -= 1) {
+    j = Math.floor(Math.random() * i)
+    x = a[i - 1]
+    a[i - 1] = a[j]
+    a[j] = x
+  }
+}
+
+$('#team .row').each((i, e) => {
+  const cards = ($(e).find('li'))
+  shuffle(cards)
+  $(e).html(cards)
+})
