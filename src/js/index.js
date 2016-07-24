49c6229b31be9a15c59342c821686e663725f291
James Martinez <jamescmartinez@gmail.com>
2016-07-24 13:18:43 -0700

    ESLint no-param-reassign

--- src/js/index.js
+++ src/js/index.js
@@ -5,14 +5,15 @@ $('.down-arrow').click(() => {
 })
 
 function shuffle(a) {
+  const aa = a
   let j
   let x
   let i
-  for (i = a.length; i; i -= 1) {
+  for (i = aa.length; i; i -= 1) {
     j = Math.floor(Math.random() * i)
-    x = a[i - 1]
-    a[i - 1] = a[j]
-    a[j] = x
+    x = aa[i - 1]
+    aa[i - 1] = aa[j]
+    aa[j] = x
   }
 }
 
